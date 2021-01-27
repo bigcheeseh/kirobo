7f684ce67b98ef0330f56891539abc72e3ff470c
Sergey Frolov <sergey.frolov@trucknet.io>
2021-01-27 20:11:30 +0200

    feat(wallet): ethers walletlogic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DeviceInfo from "./Features/DeviceInfo"
+import Web3Account from "./Features/Web3Account"
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <DeviceInfo />
+        <Web3Account />
       </header>
     </div>
   );
